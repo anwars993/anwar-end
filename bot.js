28f1cb8a232e0931efa916d70d51f06c41cb1d8b
anwars993 <56355710+anwars993@users.noreply.github.com>
2019-10-16 03:39:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`S2ud`,"http://twitch.tv/S-F")
+client.user.setGame(`https://discord.gg/rtZ9YHu`,"https://discord.gg/rtZ9YHu")
 client.user.setStatus("dnd")
 });
 
